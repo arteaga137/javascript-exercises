93db2f1a9ccded4af98410cd41bdddb9a7e7c2c7
arteaga137 <francisco.arteaga137@gmail.com>
2023-11-11 15:27:32 +0100

    second try to push map exercises

---
+++ S7/4.map/exercise-2.js
@@ -0,0 +1,14 @@
+// Dado el siguiente array, devuelve una lista que contenga los valores de la propiedad .name y cambia el nombre a 'Anacleto' en caso de que empiece por 'A'.
+
+
+const users = [{id: 1, name: 'Abel'}, {id:2, name: 'Julia'},{id:3, name: 'Pedro'}, {id:4, name: 'Amanda'}];
+
+const newUsers = users.map(user => {
+    if (user.name.startsWith('A')) {
+        return 'Anacleto';
+    } else {
+        return user.name;
+    }
+});
+
+console.log(newUsers);
---
+++ S7/4.map/exercise-3.js
@@ -0,0 +1,15 @@
+// Dado el siguiente array, devuelve una lista que contenga los valores de la propiedad .name y añade al valor de .name el string ' (Visitado)' cuando el valor de la propiedad isVisited = true.
+
+
+const cities = [{isVisited:true, name: 'Tokyo'}, {isVisited:false, name: 'Madagascar'},{isVisited:true, name: 'Amsterdam'}, {isVisited:false, name: 'Seul'}];
+
+const visitedCities = cities.map(city => {
+    if (city.isVisited) {
+        return city.name + '(Visitado)';
+    } else {
+        return city.name;
+    }
+});
+
+console.log(visitedCities);
+
